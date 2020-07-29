0fdae67439146acfaff2d74cddcfa9da7412236c
Alexandre Bontinck <alexandre.bontinck@capgemini.com>
2020-07-29 14:34:20 +0200

    mobile webdesign ok

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,54 @@
 <template>
   <div id="app">
-    <img alt="neck logo" class="neckLogo" src="./assets/images/nec-exp.png">
-    <img alt="body logo" class="bodyLogo" src="./assets/images/bod-expl.png">
-    <HelloWorld msg="Guitar configurator !"/>
+    <div class="logo">
+      <img alt="neck logo" class="neckLogo" src="./assets/images/nec-exp.png">
+      <img alt="body logo" class="bodyLogo" src="./assets/images/bod-expl.png">
+    </div>
+    <!-- <HelloWorld msg="Guitar configurator !"/> -->
+    <GuitarBuilder />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import GuitarBuilder from './components/GuitarBuilder.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
+    GuitarBuilder,
   },
 };
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  margin:0 auto;
+  padding: 0;
+  background-image: url('./assets/images/backSite-mob.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  height:100vh;
+}
 #app {
+  width:100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  padding-top: 15px;
+  margin: 0 auto;
+}
+.logo {
+  display: inline-block;
+  transform: rotate(65deg);
+  margin-top:-60px;
 }
 .neckLogo {
   position: relative;
---
+++ src/components/GuitarBuilder.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div class="builderTitle">
+      <h2>Create your own guitar !</h2>
+    </div>
+
+    <div class="mainBuilder">
+      <div class="detailsPart">
+        <ul>
+          <li>Name :</li>
+          <li>Neck :</li>
+          <li>Body</li>
+        </ul>
+      </div>
+      <div class="guitarPart">
+          <div class="necPart">
+            <img src="../assets/images/nec-exp.png" alt="neck-exp image">
+            <button class="prev-selector">&#9668;</button>
+            <button class="next-selector">&#9658;</button>
+          </div>
+          <div class="bodyPart">
+            <img src="../assets/images/bod-expl.png" alt="body-exp image">
+            <button class="prev-selector">&#9668;</button>
+            <button class="next-selector">&#9658;</button>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GuitarBuilder',
+};
+</script>
+
+<style scoped>
+    .builderTitle {
+        margin-top: -40px;
+    }
+    h2 {
+        font-size : 18px;
+        text-transform: uppercase;
+    }
+    .mainBuilder {
+        display: flex;
+        justify-content: center;
+        padding: 0 10px;
+        margin-top: 40px;
+    }
+    .detailsPart {
+        width: 35%;
+        height: 130px;
+        text-align: center;
+        padding: 25px 15px;
+        margin-right: 5%;
+    }
+    .detailsPart ul {
+        list-style: none;
+    }
+    .detailsPart ul li {
+        font-size: 13px;
+        padding: 5px 0;
+    }
+    .guitarPart {
+        width: 60%;
+    }
+    .necPart, .bodyPart {
+        position: relative;
+    }
+    .necPart img {
+        width:40%;
+    }
+    .bodyPart {
+        margin-top: -58px;
+    }
+    .bodyPart img {
+        width:80%;
+        margin-left:-19px;
+        margin-top:-3px;
+    }
+    .prev-selector {
+        position: absolute;
+        top: 80px;
+        left: 0;
+    }
+    .next-selector {
+        position: absolute;
+        top: 80px;
+        right: 0;
+    }
+
+</style>
